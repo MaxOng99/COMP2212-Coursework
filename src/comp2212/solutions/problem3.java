fe4a191702e4e2b6a981d92905912c31f9d6790c
yco1g18 <yco1g18@soton.ac.uk>
2020-03-10 12:37:09 +0000

    Solved problem 3

--- src/comp2212/solutions/problem3.java
+++ src/comp2212/solutions/problem3.java
@@ -1,10 +1,29 @@
 package comp2212.solutions;
 
+import java.util.ArrayList;
+
 public class problem3 {
 
     public static void main(String[] args) throws Exception {
         InputReader ir = new InputReader();
-        ir.readFile("./src/test/input3.txt");
-    }
+        ArrayList<ArrayList<Integer>> inputList = ir.readFile("./src/test/input3.txt");
 
+        ArrayList<Integer> firstList = inputList.get(0);
+        ArrayList<Integer> secondList = inputList.get(1);
+        ArrayList<Integer> shuffleList = new ArrayList<Integer>();
+
+        if (secondList.size() >= 2) {
+            shuffleList.add(0);
+            shuffleList.add(secondList.remove(1));
+            shuffleList.add(firstList.remove(0));
+
+            while (secondList.size() >= 3) {
+                secondList.remove(1);
+                shuffleList.add(secondList.remove(1));
+                shuffleList.add(firstList.remove(0));
+            }
+        }
+
+        System.out.println(shuffleList);
+    }
 }
