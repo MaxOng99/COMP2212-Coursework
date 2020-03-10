37773e12eff0a68868edc46b5eb05cc0adbf5a8d
yco1g18 <yco1g18@soton.ac.uk>
2020-03-10 11:42:36 +0000

    InputReader now stores integer sequences column wise.
    1. Edited InputReader.java so that it stores integer sequences from a
       file column by column.
    
    2. Edited input1.txt and input2.txt to match the originl input format.

--- src/comp2212/solutions/InputReader.java
+++ src/comp2212/solutions/InputReader.java
@@ -7,22 +7,24 @@ import java.util.ArrayList;
 public class InputReader {
 
     public ArrayList<ArrayList<Integer>> readFile(String filePath) throws Exception {
+
         ArrayList<ArrayList<Integer>> inputList = new ArrayList<ArrayList<Integer>>();
-
         File input = new File(filePath);
+        RandomAccessFile reader = new RandomAccessFile(input, "rw");
 
-        BufferedReader br = new BufferedReader(new FileReader(input));
+        String st = reader.readLine();
+        for (int i = 0; i < st.length(); i++) {
+            inputList.add(new ArrayList<Integer>());
+        }
 
-        String st;
-        while ((st = br.readLine()) != null) {
-            ArrayList<Integer> colList = new ArrayList<Integer>();
+        reader.seek(0);
+
+        while ((st = reader.readLine()) != null) {
             String[] splitNum = st.split(" ");
-            for (String num : splitNum) {
-                colList.add(Integer.parseInt(num));
+            for (int j = 0; j < splitNum.length; j++) {
+                inputList.get(j).add(Integer.parseInt(splitNum[j]));
             }
-            inputList.add(colList);
         }
         return inputList;
     }
-
 }
