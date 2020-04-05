6de92c4e0b3a341642d503487412e1f6aea0622d
yco1g18 <yco1g18@soton.ac.uk>
2020-04-05 17:08:47 +0800

    Edited SplEvaluator.hs and Main.hs
    1. Fixed bugs in evaluator file
    
    2. Main.hs now able to parse input stream

--- src/comp2212/Main.hs
+++ src/comp2212/Main.hs
@@ -7,7 +7,20 @@ import SplEvaluator
 
 main :: IO ()
 main = do args <- getArgs
-          content <- readFile (head args)
-          let parsedProgram = parseCalc (alexScanTokens content)
-          print (parsedProgram)
-          let result = evalLoop (convertToState parsedProgram inputSequence)
+          sourceCode <- readFile (args !! 0)
+          inputStream <- readFile (args !! 1)
+          let parsedProgram = parseCalc (alexScanTokens sourceCode)
+          let result = evalLoop (convertToState parsedProgram inputSequence) where
+              inputSequence = generateInputSequences $ splitCol $ splitRow inputStream
+          print (result)
+
+splitRow :: String -> [String]
+splitRow xs = lines xs
+
+splitCol :: [String] -> [[String]]
+splitCol [] = []
+splitCol (x:xs) = words x:splitCol xs
+
+generateInputSequences :: [[String]] -> [[Int]]
+generateInputSequences [] = []
+generateInputSequences xss = [(read x) :: Int | (x:xs) <- xss]:generateInputSequences (map tail xss) 
--- src/comp2212/SplEvaluator.hs
+++ src/comp2212/SplEvaluator.hs
@@ -1,6 +1,6 @@
 module SplEvaluator where
 
-import Systems.Environment
+import System.Environment
 import Tokens
 import Grammar
 import Data.List
@@ -12,9 +12,9 @@ type Environment = [(Types, String, Exp)]
 type State = (Construct, [Construct], Environment)
 
 evalLoop :: State -> String
-evalLoop state = value where
+evalLoop state = show value where
     ((Return var), rest, env) = evalConstruct state
-    (type, var, value) = getVarTuple var env
+    (t, name, value) = getVarTuple var env
 
 -- Single-step evaluation for expressions
 evalConstruct :: State -> State
@@ -33,8 +33,8 @@ evalConstruct ((While exp c), rest, e)
     | evalExp exp e == BoolTrue = evalConstruct (head new_cons, tail new_cons, e)
     | evalExp exp e == BoolFalse = evalConstruct (head rest, tail rest, e) 
     | otherwise = error "While condition not a boolean" where
-    	new_cons = cons_list ++ [While exp c] ++ rest
-    	cons_list = formatConstruct c
+        new_cons = cons_list ++ [While exp c] ++ rest
+        cons_list = formatConstruct c
 
 -- IntDeclare Evaluation [Choose between default 0 value or null]
 evalConstruct ((IntDeclare var), rest, e) = evalConstruct ((head rest), (tail rest), (SplInt, var, Int 0):e)
@@ -43,20 +43,33 @@ evalConstruct ((IntDeclare var), rest, e) = evalConstruct ((head rest), (tail re
 evalConstruct ((BoolDeclare var), rest, e) = evalConstruct ((head rest), (tail rest), (SplBool, var, BoolFalse):e)
 
 
--- VarAssign Evaluation [Might not work] [Split evalconstructs]
-evalConstruct ((VarAssign var exp), rest, e)
-    | lookUpType var e == SplBool && (evaluatedExp == BoolTrue || evaluatedExp == BoolFalse) = evalConstruct ((head rest), (tail rest), updatedEnv) 
-    | lookUpType var e == SplInt && evaluatedExp == Int num = evalConstruct ((head rest), (tail rest), updatedEnv)
-    | lookUpType var e == SplDoubleList && evaluatedExp == Sequences = evalConstruct ((head rest), (tail rest), updatedEnv)
-    | otherwise = error "Wrong data type" where 
-    	updatedEnv = update var evaluatedExp e
-        evaluatedExp = evalExp exp e
+-- IntAssign Evaluation
+evalConstruct ((VarAssign var (Int val)), rest, e)
+    | lookUpType var e == SplInt = evalConstruct ((head rest), (tail rest), update var (Int val) e)
+    | otherwise = error "Cannot assign this variable to an int"
 
+-- BoolAssign Evaluation
+evalConstruct ((VarAssign var (BoolTrue)), rest, e)
+    | lookUpType var e == SplBool = evalConstruct ((head rest), (tail rest), update var (BoolTrue) e)
+    | otherwise = error "Cannot assign this variable to a boolean"
+
+evalConstruct ((VarAssign var (BoolFalse)), rest, e)
+    | lookUpType var e == SplBool = evalConstruct ((head rest), (tail rest), update var (BoolFalse) e)
+    | otherwise = error "Cannot assign this variable to a boolean"
+
+-- DoubleListAssign Evaluation
+evalConstruct ((VarAssign var Sequences), rest, e)
+    | lookUpType var e == SplDoubleList = evalConstruct ((head rest), (tail rest), update var Sequences e)
+    | otherwise = error "Cannot assign this variable to a Double List"
+
+-- GeneralVarAssign Evaluation
+evalConstruct ((VarAssign var exp), rest, e) = evalConstruct ((head rest), (tail rest), update var evaluatedExp e) where
+    evaluatedExp = evalExp exp e
 
 -- PushAssign Evaluation
 evalConstruct ((StackOperationAssign listVar1 (Push listVar2 exp)), rest, e) = evalConstruct ((head rest), (tail rest), updatedEnv1) where
     updatedEnv1 = updateListPush listVar1 listVar2 evaluatedExp e
-    evaluatedExp = evalExp e 
+    evaluatedExp = evalExp exp e 
 
 -- PopAssign Evaluation
 evalConstruct ((StackOperationAssign intVar (Pop listVar)), rest, e) = evalConstruct ((head rest), (tail rest), updatedEnv2) where
@@ -66,12 +79,13 @@ evalConstruct ((StackOperationAssign intVar (Pop listVar)), rest, e) = evalConst
 evalConstruct ((NewSingleList var), rest, e) = evalConstruct ((head rest), (tail rest), (SplSingleList, var, List "[]"):e)
 
 -- DoubleListDeclare Evaluation
-evalConstruct ((DoubleListDeclare var exp), rest, e) = evalConstruct ((head rest), (tail rest), (SplDoubleList, var, exp):e)
+evalConstruct ((DoubleListDeclare var exp), rest, e) = evalConstruct ((head rest), (tail rest), (SplDoubleList, var, evaluatedExp):e) where
+    evaluatedExp = evalExp exp e
 
 -- Return Evaluation
 evalConstruct ((Return var), rest, e)
-    | rest == [] && lookUpType var == SplDoubleList = ((Return var), rest, e)
-    | otherwise = error "Return type is not of the correct type. It should be of SplDoubleList"
+    | rest == [] && lookUpType var e == SplSingleList = ((Return var), rest, e)
+    | otherwise = error "Return type is not of the correct type. It should be of SplSingleList"
 
 
 evalExp :: Exp -> Environment -> Exp
@@ -87,7 +101,7 @@ evalExp (Var name) e = lookUp name e
 
 evalExp (List list) e = List list
 
-evalExp (Sequences) e = Sequences
+evalExp (Sequences) e = lookUp "sequence" e
 
 -- List length function reduction
 evalExp (Length name) e = Int (length (convertToHaskellList (lookUp name e)))
@@ -110,38 +124,36 @@ evalExp (Multiply (Int a) (Int b)) e = Int (a * b)
 evalExp (Multiply e1 e2) e = evalExp (Multiply (evalExp e1 e) (evalExp e2 e)) e
 
 -- Divide expression reduction
-evalExp (Divide (Int a) (Int b)) e = Int (a / b)
+evalExp (Divide (Int a) (Int b)) e = Int (a `div` b)
 evalExp (Divide e1 e2) e = evalExp (Divide (evalExp e1 e) (evalExp e2 e)) e
 
 -- LessThan expression reduction
 evalExp (LessThan (Int a) (Int b)) e 
-    | a < b == True = BoolTrue
+    | (a < b) == True = BoolTrue
     | otherwise = BoolFalse
 evalExp (LessThan e1 e2) e = evalExp (LessThan (evalExp e1 e) (evalExp e2 e)) e
 
 -- GreaterThan expression reduction
 evalExp (GreaterThan (Int a) (Int b)) e
-    | a > b == True = BoolTrue
+    | (a > b) == True = BoolTrue
     | otherwise = BoolFalse
 evalExp (GreaterThan e1 e2) e = evalExp (GreaterThan (evalExp e1 e) (evalExp e2 e)) e
 
 -- GreaterThanOrEqualTo expression reduction
 evalExp (GTE (Int a) (Int b)) e
-    | a >= b == True = BoolTrue
+    | (a >= b) == True = BoolTrue
     | otherwise = BoolFalse
 evalExp (GTE e1 e2) e = evalExp (GTE (evalExp e1 e) (evalExp e2 e)) e
 
 -- LessThanOrEqualTo expression reduction
 evalExp (LTE (Int a) (Int b)) e
-    | a <= b == True = BoolTrue
+    | (a <= b) == True = BoolTrue
     | otherwise = BoolFalse
 evalExp (LTE e1 e2) e = evalExp (LTE (evalExp e1 e) (evalExp e2 e)) e
 
 -- Not expression evaluation
-evalExp (Not val) e
-    | val == BoolTrue = BoolFalse
-    | val == BoolFalse = BoolTrue
-    | otherwise = error "Not a valid boolean expression"
+evalExp (Not BoolTrue) e = BoolFalse
+evalExp (Not BoolFalse) e = BoolTrue
 evalExp (Not exp) e = evalExp (Not (evalExp exp e)) e
 
 -- Lookup variable value function
@@ -154,46 +166,46 @@ lookUpType name env = get1st (getVarTuple name env)
 
 -- Update variable value within current environment
 update :: String -> Exp -> Environment -> Environment
-update var exp oldEnv = (lookUpType var oldEnv, var, exp):(delete var oldEnv)
+update var exp oldEnv = (lookUpType var oldEnv, var, exp):(delete' var oldEnv)
 
 -- Update Environment for Lists when push is called
 updateListPush :: String -> String -> Exp -> Environment -> Environment
-updateListPush listVar1 listVar2 (Int val) oldEnv =
+updateListPush listVar1 listVar2 (Int val) oldEnv
     | (lookUp listVar1 oldEnv) == (lookUp listVar2 oldEnv) = update listVar1 newListExp oldEnv
     | otherwise = union (update listVar1 newListExp oldEnv) (update listVar2 newListExp oldEnv) where
         newListExp = List (show pushedList)
-        pushedList = val:(convertToHaskellList $ lookUp (listVar2 oldEnv))
+        pushedList = val:(convertToHaskellList $ lookUp listVar2 oldEnv) 
 updateListPush _ _ _ _ = error "Attempting to push non-integer value into list"
 
 -- Update Environment for Lists when pop is called
 updateListPop :: String -> String -> Environment -> Environment
 updateListPop intVar listVar oldEnv = union (update intVar intExp oldEnv) (update listVar newListExp oldEnv) where
-    intExp = x
+    intExp = Int (head poppedList)
     newListExp = List (show poppedList)
-    poppedList = case (convertToHaskellList $ lookUp (listVar oldEnv)) of 
-        [] = error "Cannot pop from an empty list"
-        (x:xs) = xs
+    poppedList = case (convertToHaskellList $ lookUp listVar oldEnv) of 
+        [] -> error "Cannot pop from an empty list"
+        (x:xs) -> xs
 
 -- Delete variable from given environment
-delete :: String -> Environment -> Environment
-delete _ [] = []
-delete var (x:xs)
-   | var == get2nd x = [] ++ (delete var xs)
-   | var /= get2nd x = [x] ++ (delete var xs) 
+delete' :: String -> Environment -> Environment
+delete' _ [] = []
+delete' var (x:xs)
+   | var == get2nd x = [] ++ (delete' var xs)
+   | var /= get2nd x = [x] ++ (delete' var xs) 
 
 -- Variable tuple get function
-getVarTuple :: String -> Environment -> (Type, String, Exp)
+getVarTuple :: String -> Environment -> (Types, String, Exp)
 getVarTuple name (x:xs)
     | name == (get2nd x) = x
     | name /= (get2nd x) = getVarTuple name xs
     | otherwise = error "No variable found"
 
 -- Convert String List to Haskell List Type
-convertToHaskellList :: String -> [Int]
-convertToHaskellList stringList
-    | length haskellList == 0 = [] 
+convertToHaskellList :: Exp -> [Int]
+convertToHaskellList (List stringList)
+    | length haskellList == 0 = []
     | otherwise = haskellList where 
-    haskellList = convert [x | x <- stringList, x /= '[', x/=']', x/= ',]'
+    haskellList = convert [x | x <- stringList, x /= '[', x/=']', x/= ',']
 
 -- Converts strings to integers
 convert :: String -> [Int]
@@ -210,7 +222,7 @@ formatConstruct x = [x]
 
 -- Parses source program and input number sequence and converts it into a state
 convertToState :: Construct -> [[Int]] -> State
-convertToState cons inpList = (head (formatConstruct cons), tail (formatConstruct cons), [])
+convertToState cons inpList = (head (formatConstruct cons), tail (formatConstruct cons), [(SplDoubleList, "sequence", Sequences (show inpList)])
 
 get1st (a,_,_) = a
 
